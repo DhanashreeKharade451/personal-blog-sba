2e876280918eea571f96b7da064f5085d468e3c9
Dhanashree <dhanashreeakharade@gmail.com>
2026-01-25 23:20:36 -0500

    Struggling with functions

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let posts = [];
+let editPosts = [];
 
-const title = document.getElementById("title");
-const  content = document.getElementById("content");
+const blogForm = document.getElementById("blogForm");
+const contentInput = document.getElementById("content");
+const titleInput = document.getElementById("title");
+const titleError = document.getElementById("titleError");
+const contentError = document.getElementById("contentError");
+const postContainer = document.getElementById("postContainer";
+const ButtonSubmit = document.getElementById("submit")
+)
+
 
 function savepost(){
      localStorage.setItem("tasks", JSON.stringify(tasks));
 }
-
